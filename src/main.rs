e6b4f1e9a0ea692fb497f99170627f26416fa377
jan.ehehalt <jan.ehehalt@telekom.de>
2025-03-14 23:44:49 +0100

    MÖRGE

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use raylib::prelude::*;
 
 use audiomanager::AudioManager;
 
+mod texture_atlas;
+use texture_atlas::TextureAtlas;
+
 fn main() {
     let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
 
@@ -16,11 +19,17 @@ fn main() {
 
     println!("({}, {})", l.width, l.height);
 
+    let mut atlas = TextureAtlas::new();
+    let mut textures: Vec<&Texture2D> = Vec::new();
+    let texture = atlas.get_texture(&mut rl, &thread, "Hammer.png").unwrap();
+    textures.push(texture);
+
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(Color::WHITE);
         d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+        d.draw_texture(textures[0], 100, 100, Color::WHITE);
         d.draw_texture(&l, 200, 200, Color::WHITE);
     }
 }
