af669fae7145290167e4be642a3646ba1c7d3add
Jonathan Hager <mail@hager-jonathan.de>
2025-03-15 21:56:44 +0100

    bin auf main WTH

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ mod texture_atlas;
 use texture_atlas::TextureAtlas;
 
 mod tiled_map;
-use tiled_map::{SCALE, TILE_HEIGHT, TILE_WIDTH, Tags, TiledMap};
+use tiled_map::{MazeConfig, SCALE, TILE_HEIGHT, TILE_WIDTH, Tags, TiledMap};
 
 mod item;
 use item::Item;
@@ -22,6 +22,10 @@ use item::Item;
 fn main() {
     let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
 
+    let test: MazeConfig = match MazeConfig::new("assets/maze1.KB") {
+        Ok(config) => config,
+        Err(why) => panic!("{}", why),
+    };
     // Hier alle Texturen einfügen, die automatisch geladen werden sollen
     // Sie können dann später mit atlas.get_texture("pfad/zu/texture") abgerufen werden
     let textures = [
@@ -55,6 +59,7 @@ fn main() {
         "assets/water1.png",
         "assets/water2.png",
         "assets/water3.png",
+        "assets/Sandmauer.png",
     ];
     let mut atlas = TextureAtlas::new();
     for path in textures.iter() {
@@ -105,7 +110,11 @@ fn main() {
     let mut frame_times = 0 as f32;
 
     // TILED MAP
-    let mut tiled_map: TiledMap<'_> = TiledMap::new(5, 20, 20, &atlas);
+    // let mut tiled_map: TiledMap<'_> = TiledMap::new(5, 20, 20, &atlas);
+    let mut tiled_map = match TiledMap::from(test, &atlas) {
+        Ok(map) => map,
+        Err(why) => panic!("Error: {}", why),
+    };
 
     // ITEMS
     let mut items = vec![
@@ -203,7 +212,9 @@ fn main() {
                                 Color::BLUE,
                             );
                         }
-                        tiled_map::Tile::AnimatedOnce(_, _, tags) if tags.contains(&Tags::Barrier) => {
+                        tiled_map::Tile::AnimatedOnce(_, _, tags)
+                            if tags.contains(&Tags::Barrier) =>
+                        {
                             d.draw_rectangle(
                                 pos.x as i32,
                                 pos.y as i32,
@@ -241,7 +252,9 @@ fn main() {
                                 Color::RED,
                             );
                         }
-                        tiled_map::Tile::AnimatedOnce(_, _, tags) if tags.contains(&Tags::Barrier) => {
+                        tiled_map::Tile::AnimatedOnce(_, _, tags)
+                            if tags.contains(&Tags::Barrier) =>
+                        {
                             d.draw_rectangle(
                                 pos.x as i32,
                                 pos.y as i32,
